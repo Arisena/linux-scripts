f708cd23f083870cdbfef086f2a4373f4fde3e80
Arisena <bcmccourt202@gmail.com>
2019-10-16 18:36:03 -0500

    launcher for system login

---
+++ launch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+trap ctrl_c sigint
+function ctrl_c() {
+	echo
+	echo "SIGINT RECEIVED"
+	echo "EXITING PROGRAM"
+	exit 1
+}
+
+dialog --title "Launch.sh" \
+	--backtitle 'Version 0.1' \
+	--cancel-label 'Exit' \
+	--msgbox 'Work in Progress, Proceed at your own risk' 7 25
+
+exec 3>&1
+selection=$(dialog --title "Graphical Interface" \
+	--backtitle 'WIP' \
+	--clear \
+	--cancel-label 'Exit' \
+	--menu "Please Select:" 11 25 4 \
+	"1" "i3" \
+	"2" "gnome" \
+	"3" "dwarf fortress" \
+	"4" "cataclysm dda" \
+	2>&1 1>&3)
+
+case $selection in
+	0 )
+		clear
+		echo "Program Exit"
+		;;
+	1 )
+		XS=i3 startx
+		;;
+	2 )
+		XS=gnome startx
+		;;
+	3 )
+		XS=dwarf startx
+		;;
+	4 )
+		XS=cata startx
+		;;
+esac
