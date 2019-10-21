acc8c4d7e9bab5266cfc141f24713ee8520f881f
Arisena <34319484+Arisena@users.noreply.github.com>
2019-10-21 13:35:52 -0500

    Removed Traps

--- launch.sh
+++ launch.sh
@@ -2,28 +2,6 @@
 
 [ -z $CALLED_FROM_START_APP ] && { echo "Not called correctly"; exit 42; }
 
-redo() {
-	for i in `seq 1 5`; do
-    	sleep .1
-    	echo -n "."
-	done
-	kill $0 2> /dev/null
-	exec $0
-}
-function ctrl_c() {
-	echo
-	echo "SIGINT RECEIVED"
-	echo "EXITING PROGRAM"
-	exit 1
-}
-trap ctrl_c sigint
-function ctrl_z() {
-	echo
-	echo "Sleep not supported"
-	redo
-}
-trap ctrl_z sigtstp
-
 dialog --title "Launch.sh" \
 	--backtitle 'Version 0.3' \
 	--cancel-label 'Exit' \
