e3b162da25bfeab178254bec68ff16c839568d8f
Arisena <34319484+Arisena@users.noreply.github.com>
2019-10-21 12:57:21 -0500

    Updated

--- launch.sh
+++ launch.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-trap ctrl_c sigint
+redo() {
+	for i in `seq 1 5`; do
+    	sleep .1
+    	echo -n "."
+	done
+	kill $0 2> /dev/null
+	exec $0
+}
 function ctrl_c() {
 	echo
 	echo "SIGINT RECEIVED"
 	echo "EXITING PROGRAM"
 	exit 1
 }
+trap ctrl_c sigint
+function ctrl_z() {
+	echo
+	echo "Sleep not supported"
+	redo
+}
 
 dialog --title "Launch.sh" \
-	--backtitle 'Version 0.1' \
+	--backtitle 'Version 0.3' \
 	--cancel-label 'Exit' \
 	--msgbox 'Work in Progress, Proceed at your own risk' 7 25
 
@@ -21,8 +34,8 @@ selection=$(dialog --title "Graphical Interface" \
 	--menu "Please Select:" 11 25 4 \
 	"1" "i3" \
 	"2" "gnome" \
-	"3" "dwarf fortress" \
-	"4" "cataclysm dda" \
+	"3" "Dwarf Fortress" \
+	"4" "Cataclysm DDA" \
 	2>&1 1>&3)
 
 case $selection in
