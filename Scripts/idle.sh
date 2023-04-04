61982e5ba4de4faacaba8913b659d853514b90c5
Brendan McCourt <bcmccourt202@gmail.com>
2023-04-03 20:52:54 -0500

    Adjusted timings, testing done

--- Scripts/idle.sh
+++ Scripts/idle.sh
@@ -2,6 +2,6 @@
 
 swayidle -w \
    timeout 300 'swaylock -f -c 000000' \
-   timeout 5 'hyprctl dispatch dpms off' \
+   timeout 600 'hyprctl dispatch dpms off' \
    resume 'hyprctl dispatch dpms on' \
    before-sleep 'swaylock -f -c 000000'
