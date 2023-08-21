10a169484fb743f6c96a7ef5c16c4141e6213be9
Brendan McCourt <bcmccourt202@gmail.com>
2023-08-21 18:13:28 -0500

    Added Comments Explaining Script

--- clear-config-DDDA.sh
+++ clear-config-DDDA.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Used for clearing DDDA Config.ini ; Seems to be cause by ALT in gnome not being ALT, but having another name
+#Still works in game, but options menu will show a blank key
 
 rm '/mnt/alt-drive-3/SteamLibrary/steamapps/compatdata/367500/pfx/drive_c/users/steamuser/AppData/Local/CAPCOM/DRAGONS DOGMA DARK ARISEN/config.ini'
 
