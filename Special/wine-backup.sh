b8784ff6941d31c5703a6ec0fbace55c4dda84be
Brendan McCourt <bmccourt@atswarra.com>
2023-11-24 23:14:05 -0600

    No Longer Backs up Vortex Folders

--- Special/wine-backup.sh
+++ Special/wine-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tar -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
+tar --exclude='*vortex*' --exclude='*Vortex*' -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
 mv -v /mnt/network/file-store/wine-backup.tar.xz /mnt/network/file-store/wine-backup-old.tar.xz
 mv -v $HOME/wine-backup.tar.xz /mnt/network/file-store/
