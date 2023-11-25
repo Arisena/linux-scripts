769f3f00b11a2fcaafce9e3d971034d9ddf99fda
Brendan McCourt <bmccourt@atswarra.com>
2023-11-25 16:03:50 -0600

    Updated Commands to use $USER Variable

--- Special/wine-backup.sh
+++ Special/wine-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#tar --exclude='*vortex*' --exclude='*Vortex*' -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
-tar --exclude='*vortex*' -cvJf $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
+#tar --exclude='*vortex*' --exclude='*Vortex*' -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/$USER/AppData
+# Using just regular tar, as it is slower when using 'xz' in the command above
+tar --exclude='*vortex*' -cvJf $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/$USER/AppData
 mv -v /mnt/network/file-store/wine-backup.tar.xz /mnt/network/file-store/wine-backup-old.tar.xz
 mv -v $HOME/wine-backup.tar.xz /mnt/network/file-store/
