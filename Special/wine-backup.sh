e167239488fd9394c7a360b66b19e8ed6a126232
Brendan McCourt <bmccourt@atswarra.com>
2023-11-24 23:15:54 -0600

    Changed tar command, runs faster

--- Special/wine-backup.sh
+++ Special/wine-backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-tar --exclude='*vortex*' --exclude='*Vortex*' -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
+#tar --exclude='*vortex*' --exclude='*Vortex*' -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
+tar --exclude='*vortex*' --exclude='*Vortex*' -cvJf $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/ari/AppData
 mv -v /mnt/network/file-store/wine-backup.tar.xz /mnt/network/file-store/wine-backup-old.tar.xz
 mv -v $HOME/wine-backup.tar.xz /mnt/network/file-store/
