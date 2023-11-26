caed1e5dda8d2f6e36371f68d98e2c71a3f597bb
Brendan McCourt <bmccourt@atswarra.com>
2023-11-26 16:07:35 -0600

    Changed from tar.xz to tar.gz:pigz

--- Special/wine-backup.sh
+++ Special/wine-backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#tar --exclude='*vortex*' --exclude='*Vortex*' -cv -I 'xz -9 -T0' -f $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/$USER/AppData
-# Using just regular tar, as it is slower when using 'xz' in the command above
-tar --exclude='*vortex*' -cvJf $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/$USER/AppData
-mv -v /mnt/network/file-store/wine-backup.tar.xz /mnt/network/file-store/wine-backup-old.tar.xz
-mv -v $HOME/wine-backup.tar.xz /mnt/network/file-store/
+#tar --exclude='*vortex*' -cvJf $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/$USER/AppData
+tar -c --use-compress-program=pigz -vf $HOME/wine-backup.tar.gz $HOME/.wine/drive_c/users/$USER/AppData
+mv -v /mnt/network/file-store/wine-backup.tar.gz /mnt/network/file-store/wine-backup-old.tar.gz
+mv -v $HOME/wine-backup.tar.gz /mnt/network/file-store/
