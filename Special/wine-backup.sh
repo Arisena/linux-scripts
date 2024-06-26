141c979b161aa42b1b0054a2d42db7dc800b35fc
Brendan McCourt <bmccourt@atswarra.com>
2024-06-26 14:22:43 -0500

    Added exclude for Vortex to Tar command

--- Special/wine-backup.sh
+++ Special/wine-backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#tar --exclude='*vortex*' -cvJf $HOME/wine-backup.tar.xz $HOME/.wine/drive_c/users/$USER/AppData
-tar -c --use-compress-program=pigz -vf $HOME/wine-backup.tar.gz $HOME/.wine/drive_c/users/$USER/AppData
+tar -c --use-compress-program=pigz --exclude="$HOME/.wine/drive_c/users/$USER/AppData/Roaming/Vortex" -vf $HOME/wine-backup.tar.gz $HOME/.wine/drive_c/users/$USER/AppData
 mv -v /mnt/network/file-store/wine-backup.tar.gz /mnt/network/file-store/wine-backup-old.tar.gz
 mv -v $HOME/wine-backup.tar.gz /mnt/network/file-store/
